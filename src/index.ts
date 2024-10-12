ee2b89f47bdad462cb94253a2a023961bf31364b
locnelor <locnelor@icloud.com>
2024-10-12 15:35:32 +0800

    feat(utils): 新增工具函数

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import { createStitches } from '@stitches/react';
-import ReactOpenRithEditor from './ReactOpenRithEditor';
-export * from './RichEditor';
-export type { RichEditorProps, RichEditorType } from './RichEditor';
-
+import { convertFromRaw, convertToRaw, EditorState } from 'draft-js';
+import ReactOpenRithEditor from './ReactOpenRichEditor';
 export { useEditorState } from './hooks/useEditorState';
+export { RichEditor } from './RichEditor';
+export type { RichEditorProps, RichEditorType } from './RichEditor';
+export { Toolbar } from './toolbars';
 export const { getCssText } = createStitches();
 export default ReactOpenRithEditor;
+
+export const stateToRaw = (editorState: EditorState) => {
+  return convertToRaw(editorState.getCurrentContent());
+};
+export const rawToState = (raw: any) => {
+  return EditorState.createWithContent(convertFromRaw(raw));
+};
